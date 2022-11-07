a1c5695b95ee04582dc9b9c6018baa3c0a37e7df
Belaoud Abdelhalim <abdelhalimbelaoud@gmail.com>
2022-11-07 14:47:04 +0100

    Third Problem

---
+++ EasyProblems/ThirdThreeNumber.java
@@ -0,0 +1,22 @@
+package EasyProblems;
+
+import java.util.Scanner;
+
+public class ThirdThreeNumber {
+
+    public static void solve(int number) {
+        if(number % 2 == 0)
+            System.out.println(number / 2 + " " + number / 2 + " 0");
+        else
+            System.out.println(-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int tests = scanner.nextInt();
+
+        for(int i = 0; i < tests; i++)
+            ThirdThreeNumber.solve(scanner.nextInt());
+    }
+
+}
