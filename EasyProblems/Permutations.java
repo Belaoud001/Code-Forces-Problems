1f2d9df860b86ecc9b0af5b36a721f7b1d3499ff
Belaoud Abdelhalim <abdelhalimbelaoud@gmail.com>
2022-11-08 18:38:26 +0100

    sixth Problem

---
+++ EasyProblems/Permutations.java
@@ -0,0 +1,29 @@
+package EasyProblems;
+
+import java.util.Scanner;
+
+public class Permutations {
+
+    static void solve(int n) {
+        StringBuilder permutation = new StringBuilder();
+
+        if (n < 4 && n > 1) {
+            System.out.println("NO SOLUTION");
+            return;
+        }
+
+        for (int i = 2; i <= n; i += 2)
+            permutation.append(i + " ");
+
+
+        for (int i = 1; i <= n; i += 2)
+            permutation.append(i + " ");
+
+        System.out.println(permutation);
+    }
+
+    public static void main(String[] args) {
+        solve(new Scanner(System.in).nextInt());
+    }
+
+}
