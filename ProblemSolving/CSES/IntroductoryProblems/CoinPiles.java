abbd409e613236d478e27e002303c59cce4ba6fc
Belaoud Abdelhalim <abdelhalimbelaoud@gmail.com>
2022-11-13 16:22:28 +0100

    Introductory Problems

--- ProblemSolving/CSES/IntroductoryProblems/CoinPiles.java
+++ ProblemSolving/CSES/IntroductoryProblems/CoinPiles.java
@@ -1,8 +1,5 @@
 package ProblemSolving.CSES.IntroductoryProblems;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class CoinPiles {
@@ -10,8 +7,23 @@ public class CoinPiles {
     static StringBuilder result = new StringBuilder();
 
     public static String solve(long x, long y) {
-       //TODO
-        return "";
+        if ((x + y != 0 && (x == 0 || y == 0)) || (x + y) % 3 != 0)
+            return "NO";
+
+        if (x == y)
+            return "YES";
+        while (true) {
+            long temp = ((x + y) * 2) / 3;
+            x = Math.max(x, y);
+
+            if (x == temp)
+                return "YES";
+            if (x > temp)
+                return "NO";
+
+            x = temp - x;
+            y = x * 2;
+        }
     }
 
     public static void main(String[] args) {
