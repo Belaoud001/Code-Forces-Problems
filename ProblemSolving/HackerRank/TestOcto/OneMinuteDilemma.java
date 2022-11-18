d3c3b135b14c8c5ac83bcd4d0d783b4b81171992
Belaoud Abdelhalim <abdelhalimbelaoud@gmail.com>
2022-11-18 21:52:44 +0100

    Hackerrank Test

---
+++ ProblemSolving/HackerRank/TestOcto/OneMinuteDilemma.java
@@ -0,0 +1,28 @@
+package ProblemSolving.HackerRank.TestOcto;
+
+public class OneMinuteDilemma {
+
+    public static int solve(int[] arr) {
+        int[] hashTable = new int[60];
+        int counter = 0;
+
+        for (int number : arr)
+            hashTable[number % 60]++;
+
+        // nC2 = n * (n - 1) / 2
+        counter += hashTable[0]  * (hashTable[0] - 1)  / 2;
+        counter += hashTable[30] * (hashTable[30] - 1) / 2;
+
+        for (int i = 1; i < 30; i++)
+            counter += hashTable[i] * hashTable[60 - i];
+
+        return counter;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{20, 40, 60, 60, 60};
+
+        System.out.println(solve(arr)); //4
+    }
+
+}
