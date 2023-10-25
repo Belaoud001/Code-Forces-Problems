5a00cfa3f7256a807a509cbe48fe79aabf70e6b7
m2gi-belaouda <abdelhalimbelaoud@gmail.com>
2023-10-25 15:00:37 +0200

    [CSES] Solve distinct numbers problem.

---
+++ ProblemSolving/CSES/SortingAndSearching/DistinctNumbers.java
@@ -0,0 +1,21 @@
+package ProblemSolving.CSES.SortingAndSearching;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class DistinctNumbers {
+
+    public static int solve(List<Integer> numbers) {
+        return new HashSet<>(numbers).size();
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                                                                .map(Integer::parseInt)
+                                                                .collect(Collectors.toList());
+
+        System.out.println(solve(numbers));
+    }
+
+}
--- ProblemSolving/RandomProblems/BalancedParentheses.java
+++ ProblemSolving/RandomProblems/BalancedParentheses.java
@@ -2,23 +2,20 @@ package ProblemSolving.RandomProblems;
 
 import java.util.Scanner;
 
-// In this problem we are allowed to make a string that contain one type of parentheses as input.
 public class BalancedParentheses {
     public static boolean solve(String parentheses) {
-        // Here we are at the balanced state
         int levelDetector = 0;
 
         for (int i = 0; i < parentheses.length(); i++) {
-            if (levelDetector < 0)
-                return false;
-
             if (parentheses.charAt(i) == '(')
                 levelDetector++;
             else
                 levelDetector--;
+
+            if (levelDetector < 0)
+                return false;
         }
 
-        // We must always head to the balanced state.
         return levelDetector == 0;
     }
 
